0e1959206f85158f432988bc610044bd2bd1f50e
daniilzastaskov <znata_2014@mail.ru>
2022-07-05 19:36:46 +0300

    Menu View

---
+++ src/main/java/com/example/courseproject1/Menu.java
@@ -0,0 +1,49 @@
+package com.example.courseproject1;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Menu {
+
+    @FXML
+    private Button QuotesButton;
+
+    @FXML
+    private Button accountButton;
+
+    @FXML
+    private Button createQuoteButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Button myQuotesButton;
+
+    @FXML
+    void QuotesButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accountButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createQuoteButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exitButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void myQuotesButtonListener(ActionEvent event) {
+
+    }
+
+}
